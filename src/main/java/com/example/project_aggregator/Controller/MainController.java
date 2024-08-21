7c3f4e65e994ed234c8661bce32db4364fc5c84f
first <castrojonatan24@gmail.com>
2024-08-21 16:35:20 -0400

    Modified directory structure and thymeleaf replace works. Need to make it so that the page is inherited

--- src/main/java/com/example/project_aggregator/Controller/MainController.java
+++ src/main/java/com/example/project_aggregator/Controller/MainController.java
@@ -1,12 +1,32 @@
 package com.example.project_aggregator.Controller;
 
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+import org.thymeleaf.TemplateEngine;
 
 @RestController
 public class MainController {
+
     @GetMapping("/")
-    String greetings(){
+    public ModelAndView templates(Model model){
+        ModelAndView mav = new ModelAndView();
+        mav.setViewName("HeaderAndSidebarTemplate");
+        model.addAttribute("Test", "thymeleaf replace");
+        return mav;
+    }
+
+    @GetMapping("/helloworld")
+    public String helloworld(){
         return "HelloWorld";
+    }
+
+    @GetMapping("/headertemplate")
+    public ModelAndView headertemplate(Model model){
+        ModelAndView mav = new ModelAndView();
+        mav.setViewName("HeaderAndSidebarTemplate");
+        model.addAttribute("Test", "thymeleaf replace");
+        return mav;
     }
 }
