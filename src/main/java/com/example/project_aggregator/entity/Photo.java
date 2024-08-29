11e5c859f0edd001c2640cb96939249487b67d7b
first <castrojonatan24@gmail.com>
2024-08-28 20:22:59 -0400

    NOw we can load a base64image to use on the img html element src

--- src/main/java/com/example/project_aggregator/entity/Photo.java
+++ src/main/java/com/example/project_aggregator/entity/Photo.java
@@ -25,4 +25,52 @@ public class Photo {
     @ManyToOne(targetEntity = Item.class, optional = false)
     @JoinColumn(name = "item_id", nullable = false)
     Item item;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getFilename() {
+        return filename;
+    }
+
+    public void setFilename(String filename) {
+        this.filename = filename;
+    }
+
+    public String getTimestamp_created() {
+        return timestamp_created;
+    }
+
+    public void setTimestamp_created(String timestamp_created) {
+        this.timestamp_created = timestamp_created;
+    }
+
+    public String getTimestamp_edited() {
+        return timestamp_edited;
+    }
+
+    public void setTimestamp_edited(String timestamp_edited) {
+        this.timestamp_edited = timestamp_edited;
+    }
+
+    public String getTimestamp_deleted() {
+        return timestamp_deleted;
+    }
+
+    public void setTimestamp_deleted(String timestamp_deleted) {
+        this.timestamp_deleted = timestamp_deleted;
+    }
+
+    public Item getItem() {
+        return item;
+    }
+
+    public void setItem(Item item) {
+        this.item = item;
+    }
 }
--- src/main/java/com/example/project_aggregator/service/PhotoService.java
+++ src/main/java/com/example/project_aggregator/service/PhotoService.java
@@ -5,9 +5,17 @@ import com.example.project_aggregator.entity.Photo;
 import com.example.project_aggregator.repository.PhotoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.util.Base64Utils;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
 
 @Service
 public class PhotoService {
+
+
+    private final String photoFolderPath = "./uploads/photos/";
 
     private final PhotoRepository photoRepository;
 
@@ -19,5 +27,25 @@ public class PhotoService {
     public Photo retrievePhotoForItem(Item item){
         Photo photo = this.photoRepository.findByItem(item);
         return photo;
+    }
+
+    public String retrievePhotoBase64ForItem(Item item){
+        /* ONLY TO PRINT THE CURRENT DIRECTORY
+        String currentDirectory = System.getProperty("user.dir");
+        // Print the current working directory
+        System.out.println("Current working directory: " + currentDirectory);
+        */
+        String prefix = "data:image/jpeg;base64,";
+        Photo photo = this.photoRepository.findByItem(item);
+        String filename = photo.getFilename();
+        File imgFile = new File(photoFolderPath+filename);
+        try {
+            byte[] imageBytes = Files.readAllBytes(imgFile.toPath());
+            String base64Image = Base64Utils.encodeToString(imageBytes);
+            return prefix+base64Image;
+        } catch (IOException e){
+            e.printStackTrace();
+            return e.getMessage();
+        }
     }
 }
