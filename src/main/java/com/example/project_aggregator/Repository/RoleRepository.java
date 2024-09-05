3047a141759fe9a21191b1b178318a54c503d251
first <castrojonatan24@gmail.com>
2024-09-05 15:36:10 -0400

    Added the Role and UserEntity repositories

---
+++ src/main/java/com/example/project_aggregator/Repository/RoleRepository.java
@@ -0,0 +1,10 @@
+package com.example.project_aggregator.Repository;
+
+import com.example.project_aggregator.Entity.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface RoleRepository extends JpaRepository<Role, Integer> {
+    Optional<Role> findByName(String name);
+}
---
+++ src/main/java/com/example/project_aggregator/Repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.example.project_aggregator.Repository;
+
+import com.example.project_aggregator.Entity.UserEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<UserEntity, Integer> {
+    Optional<UserEntity> findByUsername(String username);
+    Boolean existsByUsername(String username);
+}
